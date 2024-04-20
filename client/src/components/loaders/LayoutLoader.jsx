f5ad9654d15da5622257b3f1794eeade7716c0a4
shaki <developerhubytchannel@gmail.com>
2024-04-20 19:35:49 +0600

    layout loader minor update

--- client/src/components/loaders/LayoutLoader.jsx
+++ client/src/components/loaders/LayoutLoader.jsx
@@ -1,4 +1,4 @@
-import { Divider, Skeleton, Spacer } from "@nextui-org/react";
+import { Skeleton } from "@nextui-org/react";
 import React, { memo } from "react";
 
 const RandomSkeleton = ({ height, total = 100 }) => (
@@ -13,7 +13,7 @@ const RandomSkeleton = ({ height, total = 100 }) => (
 );
 
 const SidebarSkeleton = () => (
-  <div className="h-fll w-12 p-2 flex flex-shrink-0 flex-grow-0 flex-col justify-between items-center">
+  <div className="h-fll w-12 p-2 px-3 flex flex-shrink-0 flex-grow-0 flex-col justify-between items-center border-r-2 border-white/5">
     <div className="flex flex-col gap-2 justify-center items-center">
       {Array(3)
         .fill(0)
@@ -32,11 +32,15 @@ const SidebarSkeleton = () => (
 );
 
 const ChatListSkeleton = () => (
-  <div className="w-full flex-grow flex-shrink sm:w-[300px] md:w-[350px] h-full sm:flex-grow-0 sm:flex-shrink-0 flex-col gap-2 justify-start items-center overflow-hidden">
-    {Array(10)
-      .fill(0)
-      .map((_, key) => (
-        <React.Fragment key={key}>
+  <div className="w-full flex-grow flex-shrink h-full sm:flex-grow-0 sm:flex-shrink-0 flex-col gap-2 justify-start items-center overflow-hidden divide-y-2 divide-white/5">
+    <div className="flex justify-between items-center p-3 gap-2">
+      <Skeleton className="w-20 h-10 rounded-md" />
+      <Skeleton className="w-full h-10 rounded-md flex-1" />
+    </div>
+    <div className="flex flex-col divide-y-2 divide-white/5">
+      {Array(20)
+        .fill(0)
+        .map((_, key) => (
           <div
             key={key}
             className="w-full p-2 flex gap-2 justify-start items-center"
@@ -50,32 +54,27 @@ const ChatListSkeleton = () => (
               </div>
             </div>
           </div>
-          <Divider orientation="horizontal" className="" />
-        </React.Fragment>
-      ))}
+        ))}
+    </div>
   </div>
 );
 
 const ChatBoxTopSkeleton = () => (
-  <>
-    <div className="w-full flex gap-3 justify-start items-center py-3">
-      <Skeleton className="size-14 rounded-full flex-grow-0 flex-shrink-0" />
-      <div className="w-full flex flex-col gap-3">
-        <Skeleton className="w-[90%] max-w-56 h-4 rounded-full" />
-        <div className="w-full flex flex-col gap-1">
-          <Skeleton className="w-[90%] max-w-36 h-2 rounded-full" />
-          <Skeleton className="w-[90%] max-w-40 h-1 rounded-full" />
-        </div>
+  <div className="w-full flex gap-3 justify-start items-center py-3">
+    <Skeleton className="size-14 rounded-full flex-grow-0 flex-shrink-0" />
+    <div className="w-full flex flex-col gap-3">
+      <Skeleton className="w-[90%] max-w-56 h-4 rounded-full" />
+      <div className="w-full flex flex-col gap-1">
+        <Skeleton className="w-[90%] max-w-36 h-2 rounded-full" />
+        <Skeleton className="w-[90%] max-w-40 h-1 rounded-full" />
       </div>
     </div>
-    <Divider orientation="horizontal" className="" />
-    <Spacer x={1} />
-  </>
+  </div>
 );
 const ChatBoxChatSkeleton = () => (
-  <div className="w-full overflow-hidden flex justify-end items-end h-full flex-1">
+  <div className="w-full h-auto flex-1 overflow-hidden flex justify-center items-end pb-5">
     <div className="w-full flex flex-col gap-2">
-      {Array(20)
+      {Array(10)
         .fill(0)
         .map((_, key) => {
           const height = Math.round(1 + Math.random() * 2) * 35;
@@ -96,20 +95,16 @@ const ChatBoxChatSkeleton = () => (
   </div>
 );
 const ChatBoxInputSkeleton = () => (
-  <>
-    <Spacer x={1} />
-    <Divider orientation="horizontal" className="" />
-    <div className="w-full h-16 py-3 flex gap-2">
-      <Skeleton className="w-14 h-full rounded-md" />
-      <Skeleton className="w-full h-full rounded-full" />
-      <Skeleton className="w-10 h-full rounded-md" />
-      <Skeleton className="w-10 h-full rounded-md" />
-    </div>
-  </>
+  <div className="w-full h-14 py-3 flex gap-2">
+    <Skeleton className="w-14 h-full rounded-md" />
+    <Skeleton className="w-full h-full rounded-full" />
+    <Skeleton className="w-10 h-full rounded-md" />
+    <Skeleton className="w-10 h-full rounded-md" />
+  </div>
 );
 
 const ChatBoxSkeleton = () => (
-  <div className="hidden h-full w-full sm:flex flex-col px-3">
+  <div className="hidden h-screen w-full md:flex flex-col px-3 divide-y-2 divide-white/5">
     <ChatBoxTopSkeleton />
     <ChatBoxChatSkeleton />
     <ChatBoxInputSkeleton />
@@ -120,14 +115,14 @@ const LayoutLoader = () => {
   return (
     <div className="w-full h-screen flex overflow-hidden bg-foreground-50">
       <SidebarSkeleton />
-      <Divider orientation="vertical" className="" />
-      <Spacer x={1} />
-      <div className="w-full h-full flex">
-        <ChatListSkeleton />
-        <Spacer x={1} className="hidden sm:block" />
-        <Divider orientation="vertical" className="hidden sm:block" />
-        <Spacer x={1} className="hidden sm:block" />
-        <ChatBoxSkeleton />
+
+      <div className="w-full h-full grid grid-cols-12 divide-x-2 divide-white/5">
+        <div className="w-full col-span-full md:col-span-5 lg:col-span-4">
+          <ChatListSkeleton />
+        </div>
+        <div className="w-full hidden md:block md:col-span-7 lg:col-span-8">
+          <ChatBoxSkeleton />
+        </div>
       </div>
     </div>
   );
