322cd60e057f07da9ae7ccf6f43e125c04ba76a5
developerHub01 <developerhubytchannel@gmail.com>
2024-05-12 22:30:39 +0600

    gender and email field added in profileSection and also close in click out of edit box

--- src/components/specific/setting/ProfileSection.jsx
+++ src/components/specific/setting/ProfileSection.jsx
@@ -1,4 +1,10 @@
-import React, { Suspense, useRef, useState } from "react";
+import React, {
+  Suspense,
+  forwardRef,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import SettingPopupWindowLoader from "../../loaders/SettingPopupWindowLoader";
 import {
@@ -19,6 +25,7 @@ import {
 import {
   AvatarIcon,
   CheckIcon,
+  CloseIcon,
   EditIcon,
   LogoutIcon,
   ThreeDotIcon,
@@ -28,10 +35,12 @@ import { LOGOUT_API, UPDATE_USER_INFO_API } from "../../../constants/values";
 import { setUserData } from "../../../redux/slices/userAuthSlice";
 import useAuthPatch from "../../../hooks/useAuthPatch";
 import { togglePreviewImage } from "../../../redux/slices/uiStatesSlice";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { animProps1 } from "../../animation/animationList";
 
 const MAX_AVATAR_SIZE = 5 * 1024 * 1024;
+const BIO_MAX_LENGTH = 100;
+const preventEditModeList = ["gender", "email"];
 
 const avatarOptions = [
   {
@@ -47,6 +56,14 @@ const avatarOptions = [
     text: "Change Image",
   },
 ];
+
+const detailsTextList = {
+  fullName: "Full Name",
+  userName: "username",
+  email: "Email",
+  gender: "Gender",
+  bio: "Bio",
+};
 
 const ProfileAvatar = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -103,7 +120,7 @@ const ProfileAvatar = () => {
       {...animProps1}
       className="w-full flex flex-wrap justify-between items-center mb-4"
     >
-      <div className="w-20 h-20 sm:w-32 sm:h-32 rounded-full">
+      <div className="w-20 h-20 sm:w-28 sm:h-28 rounded-full">
         {isLoading ? (
           <Skeleton className="w-full h-full rounded-full" />
         ) : (
@@ -163,89 +180,113 @@ const ProfileAvatar = () => {
   );
 };
 
-const UserDetailsListItem = ({ detailsType }) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const { user } = useSelector((state) => state.authStates);
-  const detailsItemData = user[detailsType];
-
-  const dispatch = useDispatch();
-  const [isEdiable, setIsEditable] = useState(false);
-  const [updatedDetailsItemData, setUpdatedDetailsItemData] =
-    useState(detailsItemData);
-  const updateMethod = useAuthPatch();
-  const bioMaxLength = 100;
-  const handleToggleEdit = () => setIsEditable((prev) => !prev);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setIsLoading((prev) => true);
-    if (updatedDetailsItemData && updatedDetailsItemData !== detailsItemData) {
-      const data = await updateMethod(UPDATE_USER_INFO_API, {
-        [detailsType]: updatedDetailsItemData,
-      });
-      if (data?.success) {
-        dispatch(setUserData(data?.user));
-        setUpdatedDetailsItemData((prev) => data?.user?.detailsItemData);
-      } else setUpdatedDetailsItemData((prev) => detailsItemData);
-    }
-    setIsEditable((prev) => false);
-    setIsLoading((prev) => false);
-  };
-  const handleChange = (e) => {
-    detailsType === "bio"
-      ? e.target.value.length <= bioMaxLength &&
-        setUpdatedDetailsItemData(
-          (prev) => e.target.value
-        ) /* This is for preventing max length of bio text */
-      : setUpdatedDetailsItemData((prev) => e.target.value);
-  };
-
-  const handleKeyDown = (e) => e.key === "Enter" && e.preventDefault();
-
+const EditDataButton = ({ isLoading, handleOpenEdit }) => {
   return (
-    <motion.div
-      {...animProps1}
-      className="w-full py-1 flex gap-2 justify-between items-center"
-    >
-      {isEdiable ? (
-        <div className="w-full flex flex-col gap-2">
-          <form
-            onSubmit={handleSubmit}
-            className="w-full flex justify-between items-center gap-2"
+    <>
+      {isLoading ? (
+        <Skeleton className="size-10 rounded-full flex-shrink-0" />
+      ) : (
+        <Tooltip content="Edit" color="primary" radius="sm" showArrow size="sm">
+          <Button
+            type="button"
+            isIconOnly
+            radius="full"
+            size="sm"
+            color="primary"
+            onClick={handleOpenEdit}
+            className="group bg-transparent text-primary-500 hover:text-white relative"
           >
-            <React.Fragment>
+            <span className="absolute w-full h-full bg-primary-500 rounded-full z-0 scale-0 group-hover:scale-100 transition-all duration-75 opacity-0 group-hover:opacity-100"></span>
+            <EditIcon className="text-base sm:text-lg relative z-10" />
+          </Button>
+        </Tooltip>
+      )}
+    </>
+  );
+};
+
+const EditableField = forwardRef(
+  (
+    {
+      handleSubmit,
+      detailsType,
+      detailsItemData,
+      updatedDetailsItemData,
+      handleChange,
+      handleKeyDown,
+      handleCancelEdit,
+    },
+    ref
+  ) => {
+    const inputWrapperRef = useRef(null);
+    const bioLimitBox = useRef(null);
+    const saveButtonRef = useRef(null);
+
+    useEffect(() => {
+      const preventDOMList = [
+        inputWrapperRef,
+        bioLimitBox,
+        // saveButtonRef
+      ];
+      const handleClick = (e) => {
+        if (
+          inputWrapperRef?.current &&
+          preventDOMList.every((item) => !item?.current?.contains(e.target))
+        )
+          handleCancelEdit();
+      };
+
+      document.addEventListener("click", handleClick);
+
+      return () => document.removeEventListener("click", handleClick);
+    }, [inputWrapperRef]);
+
+    const inputProps = {
+      className: "w-full text-foreground-300 text-base",
+      type: "text",
+      variant: "faded",
+      color: "primary",
+      defaultValue: detailsItemData,
+      value: updatedDetailsItemData,
+      onChange: handleChange,
+    };
+
+    return (
+      <motion.div
+        key={detailsType}
+        {...animProps1}
+        className="w-full flex flex-col gap-2"
+      >
+        <form
+          onSubmit={handleSubmit}
+          className="w-full flex justify-between items-center gap-2"
+        >
+          <div className="w-full" ref={inputWrapperRef}>
+            <div className="w-full" ref={ref}>
               {detailsType === "bio" ? (
                 <Textarea
-                  className="w-full text-foreground-300 text-base"
                   placeholder="Enter your bio"
-                  type="text"
-                  variant="faded"
-                  color="primary"
-                  defaultValue={detailsItemData}
-                  value={updatedDetailsItemData}
+                  {...inputProps}
                   minRows={1}
-                  onChange={handleChange}
                   onKeyDown={handleKeyDown}
                 />
               ) : (
-                <Input
-                  className="w-full text-foreground-300"
-                  type="text"
-                  variant="faded"
-                  color="primary"
-                  defaultValue={detailsItemData}
-                  value={updatedDetailsItemData}
-                  onChange={handleChange}
-                />
+                <Input {...inputProps} />
               )}
-            </React.Fragment>
+            </div>
+          </div>
 
+          <div
+            className="flex justify-center items-center gap-2"
+            ref={saveButtonRef}
+          >
             <Tooltip
               content="Save"
               color="primary"
               radius="sm"
               showArrow
               size="sm"
+              className="pointer-events-none"
             >
               <Button
                 type="submit"
@@ -257,60 +298,124 @@ const UserDetailsListItem = ({ detailsType }) => {
                 <CheckIcon className="text-base" />
               </Button>
             </Tooltip>
-          </form>
-          {detailsType === "bio" && (
-            <div className="w-full flex justify-end items-center gap-2 select-none">
-              <Chip color="primary" variant="bordered" radius="sm">
-                {updatedDetailsItemData.length}
-              </Chip>
-              <Chip color="primary" radius="sm">
-                {bioMaxLength}
-              </Chip>
-            </div>
-          )}
-        </div>
-      ) : (
-        <React.Fragment>
-          {detailsType === "fullName" ? (
-            isLoading ? (
-              <Skeleton className="w-full h-12 rounded-md" />
-            ) : (
-              <h1 className="text-lg sm:text-xl font-bold text-foreground-200">
-                {detailsItemData}
-              </h1>
-            )
-          ) : isLoading ? (
-            <Skeleton className="w-full h-8 rounded-md" />
-          ) : (
-            <p className="text-base text-foreground-200">{detailsItemData}</p>
-          )}
+          </div>
+        </form>
+        {detailsType === "bio" && (
+          <div
+            className="w-full justify-end items-center gap-2 select-none flex flex-wrap"
+            ref={bioLimitBox}
+          >
+            <Chip color="primary" variant="bordered" radius="sm">
+              {updatedDetailsItemData.length}
+            </Chip>
+            <Chip color="primary" radius="sm">
+              {BIO_MAX_LENGTH}
+            </Chip>
+          </div>
+        )}
+      </motion.div>
+    );
+  }
+);
 
-          {isLoading ? (
-            <Skeleton className="size-10 rounded-full flex-shrink-0" />
-          ) : (
-            <Tooltip
-              content="Edit"
-              color="primary"
-              radius="sm"
-              showArrow
-              size="sm"
-            >
-              <Button
-                type="button"
-                isIconOnly
-                radius="full"
-                size="sm"
-                color="primary"
-                onClick={handleToggleEdit}
-                className="group bg-transparent text-primary-500 hover:text-white relative"
+const UserDetailsListItem = ({ detailsType }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { user } = useSelector((state) => state.authStates);
+  const detailsItemData = user[detailsType];
+  const editFieldRef = useRef(null);
+
+  const dispatch = useDispatch();
+  const [isEdiable, setIsEditable] = useState(false);
+  const [updatedDetailsItemData, setUpdatedDetailsItemData] =
+    useState(detailsItemData);
+
+  const updateMethod = useAuthPatch();
+
+  useEffect(() => {
+    if (editFieldRef?.current) {
+      editFieldRef.current.querySelector("input, textarea").focus();
+    }
+  }, [isEdiable]);
+
+  const handleOpenEdit = () => setIsEditable((prev) => !prev);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading((prev) => true);
+    if (updatedDetailsItemData && updatedDetailsItemData !== detailsItemData) {
+      const data = await updateMethod(UPDATE_USER_INFO_API, {
+        [detailsType]: updatedDetailsItemData,
+      });
+      if (data?.success) dispatch(setUserData(data?.user));
+    }
+    setIsEditable((prev) => false);
+    setIsLoading((prev) => false);
+  };
+  const handleChange = (e) => {
+    detailsType === "bio"
+      ? e.target.value.length <= BIO_MAX_LENGTH &&
+        setUpdatedDetailsItemData(
+          (prev) => e.target.value
+        ) /* This is for preventing max length of bio text */
+      : setUpdatedDetailsItemData((prev) => e.target.value);
+  };
+
+  const handleKeyDown = (e) => e.key === "Enter" && e.preventDefault();
+
+  const handleCancelEdit = () => setIsEditable((prev) => false);
+
+  const editableFieldOptions = {
+    handleSubmit,
+    detailsType,
+    detailsItemData,
+    updatedDetailsItemData,
+    handleChange,
+    handleKeyDown,
+    handleCancelEdit,
+  };
+
+  return (
+    <motion.div
+      {...animProps1}
+      className="w-full py-1 flex gap-2 justify-between items-center"
+    >
+      <AnimatePresence>
+        {isEdiable && !preventEditModeList.includes(detailsType) ? (
+          <EditableField {...editableFieldOptions} ref={editFieldRef} />
+        ) : (
+          <React.Fragment>
+            {detailsType === "fullName" ? (
+              isLoading ? (
+                <Skeleton className="w-full h-12 rounded-md" />
+              ) : (
+                <h1 className="text-lg sm:text-xl font-bold text-foreground-200 capitalize">
+                  {detailsItemData}
+                </h1>
+              )
+            ) : isLoading ? (
+              <Skeleton className="w-full h-8 rounded-md" />
+            ) : (
+              <p
+                className={`text-base text-foreground-200 ${
+                  !["gender"].includes(detailsType) ? "" : "capitalize"
+                }`}
               >
-                <span className="absolute w-full h-full bg-primary-500 rounded-full z-0 scale-0 group-hover:scale-100 transition-all duration-75 opacity-0 group-hover:opacity-100"></span>
-                <EditIcon className="text-base sm:text-lg relative z-10" />
-              </Button>
-            </Tooltip>
-          )}
-        </React.Fragment>
-      )}
+                <span className="font-bold capitalize pr-1">
+                  {detailsTextList[detailsType]} :
+                </span>
+                {detailsItemData}
+              </p>
+            )}
+
+            {!preventEditModeList.includes(detailsType) && (
+              <EditDataButton
+                isLoading={isLoading}
+                handleOpenEdit={handleOpenEdit}
+              />
+            )}
+          </React.Fragment>
+        )}
+      </AnimatePresence>
     </motion.div>
   );
 };
@@ -341,13 +446,20 @@ const LogoutButton = () => {
 const ProfileSection = () => {
   return (
     <Suspense fallback={<SettingPopupWindowLoader />}>
-      <div className="w-full flex flex-col justify-center items-center gap-2 sm:gap-4">
+      <div className="w-full flex flex-col justify-center items-center gap-2">
         <ProfileAvatar />
         <React.Fragment>
           <UserDetailsListItem detailsType="fullName" />
           <UserDetailsListItem detailsType="userName" />
+          <UserDetailsListItem detailsType="email" />
+          <UserDetailsListItem detailsType="gender" />
           <UserDetailsListItem detailsType="bio" />
         </React.Fragment>
+
+        <p className="text-center text-xs text-gray-500 dark:text-gray-200 py-3 my-0 sm:my-2">
+          To cancel edit click outside of input box
+        </p>
+
         <Divider orientation="horizontal" className="bg-primary-500/20 h-1" />
         <LogoutButton />
       </div>
